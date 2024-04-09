f52c796461a9e34e1035e8f9698c3d95c3ab7c91
Chai66 <chaitanyachalumuri06@gmail.com>
2024-04-10 01:43:15 +0530

    shell push

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@
 AMI=ami-0f3c7d07486cad139
 SG_ID=sg-0badbf0a0379ce0e9
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z02778233ILV2BIMN8413
+DOMAIN_NAME=devopspractice123.online
 
 for i in "${INSTANCES[@]}"
 do
@@ -16,6 +18,26 @@ do
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0badbf0a0379ce0e9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
+
+   # create route53 record, makesure you delete existing record
+ aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Creating a record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$i'.'$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP_ADDRESS'"
+        }]
+      }
+    }]
+  }
+    '
 done
 
 # tag specification will give name like mongodb, redis etc while creating ec2 instance 
