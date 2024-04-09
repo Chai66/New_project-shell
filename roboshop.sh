6274fb5236c9b81a9a51e9cf18c7be33da47f360
Chai66 <chaitanyachalumuri06@gmail.com>
2024-04-10 00:50:17 +0530

    shell push

--- roboshop.sh
+++ roboshop.sh
@@ -9,9 +9,9 @@ do
     echo "INSTANCE is $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then 
-        INSTANCE_TYPE = "t3.small"
+        INSTANCE_TYPE="t3.small"
     else
-        INSTANCE_TYPE = "t2.micro"
+        INSTANCE_TYPE="t2.micro"
     fi
 
     aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0badbf0a0379ce0e9
